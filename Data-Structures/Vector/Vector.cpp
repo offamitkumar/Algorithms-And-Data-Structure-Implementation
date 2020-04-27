30e4bd6117419e12fa9a3b0a62a56d0c2b86627b
Amit Kumar <offamitkumar@gmail.com>
2020-04-28 00:08:34 +0530

    Vector : added functions as push_back , pop_back , size , resize , begin , back

---
+++ Data-Structures/Vector/Vector.cpp
@@ -0,0 +1,135 @@
+#include <algorithm>
+using namespace std;
+template<typename Object> 
+class Vector{
+    private:
+        int theSize;
+        int theCapacity;
+        Object * objects;
+
+    public:
+
+        static const int SPARE_CAPACITY = 16;
+
+        // constructor 
+        explicit Vector( int initSize = 0 ): theSize{ initSize } , theCapacity{ initSize + SPARE_CAPACITY }{
+            objects = new Object[ theCapacity ];
+        }
+
+        // constructor 
+        Vector( Vector & rhs ):theSize{ rhs.theSize } , theCapacity{ rhs.theCapacity } , objects{ nullptr }{
+            objects = new Object[ theCapacity ];
+            for( int i{ 0 }; i < theSize; ++i ){
+                objects[ i ] = rhs.objects[ i ];
+            }
+        }
+
+        // operator = 
+        Vector &operator= ( const Vector &rhs ) {
+
+            Vector copy = rhs;
+            std::swap( *this , copy );
+            return *this;
+
+        }
+
+
+        // destructor 
+        ~Vector() {
+            delete []objects;
+        }
+
+
+        Vector ( Vector && rhs ): theSize{ rhs.theSize }, theCapacity{ rhs.theCapacity } , objects{ rhs.objects }{
+            rhs.objects = nullptr;
+            rhs.theSize = 0;
+            rhs.theCapacity = 0;
+        }
+
+
+        // operator =  // for r-value reference 
+        Vector &operator= ( Vector &&rhs ) {
+
+            std::swap( theSize , rhs.theSize );
+            std::swap( theCapacity, rhs.theCapacity );
+            std::swap( objects , rhs.objects );
+            return *this;
+
+        }
+
+        
+        void resize( int newSize ) {
+            if( newSize > theCapacity ) {
+                reserve( newSize * 2 ); // vector always double the memory storage 
+            }
+            theSize = newSize;
+        }
+
+
+        void reserve( int newCapacity ) {
+            if( newCapacity < theSize ) {
+                return ;
+            }
+
+            Object *newArray = new Object[ newCapacity ]; 
+
+            for( int i{0}; i < theSize; ++i ) {
+                newArray[ i ] =  std::move( objects[ i ] );
+            }
+
+            theCapacity = newCapacity;
+            std::swap( objects , newArray ); // now object have a different size 
+            delete []newArray; // delete the temporary array 
+
+        }
+
+        Object& operator[] (int index ) {
+            return objects[ index ];
+        }
+
+        const Object& operator[] (int index ) const {
+            return objects[ index ];
+        }
+
+        bool empty( void  ) const {
+            return size() == 0;
+        }
+
+        bool size( void ) const {
+            return theSize;
+        }
+
+        void push_back(const Object &x ) {
+            if( theSize == theCapacity ) {
+                reserve( 2 * theCapacity + 1 );
+            }
+            objects[ theSize++ ] = x;
+        }
+
+        void push_back( const Object &&x ) {
+            if( theSize == theCapacity ) {
+                reserve( 2 * theCapacity +1 );
+            }
+            objects[ theSize++ ] = std::move( x );
+        }
+
+        void pop_back( void ) {
+            theSize--;
+        }
+
+        const Object & back( void  ) const {
+            return objects[ theSize - 1 ];
+        }
+
+        typedef Object * iterator;
+
+        typedef const Object * const_iterator;
+
+
+};
+
+
+/***********************main function*********************************/
+int main(void){
+    return 0;
+}
