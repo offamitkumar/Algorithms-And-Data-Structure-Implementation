7b8c688a0bd1af67dde6a4061368b9dc74082180
Amit Kumar <offamitkumar@gmail.com>
2020-04-28 13:16:25 +0530

    added begin() & end() function

--- Data-Structures/Vector/Vector.cpp
+++ Data-Structures/Vector/Vector.cpp
@@ -1,5 +1,5 @@
 #include <algorithm>
-using namespace std;
+
 template<typename Object> 
 class Vector{
     private:
@@ -16,18 +16,18 @@ class Vector{
             objects = new Object[ theCapacity ];
         }
 
-        // constructor 
+        // copy constructor 
         Vector( Vector & rhs ):theSize{ rhs.theSize } , theCapacity{ rhs.theCapacity } , objects{ nullptr }{
             objects = new Object[ theCapacity ];
+            // Never assign values in assignment list , as it will do a deep copy 
             for( int i{ 0 }; i < theSize; ++i ){
-                objects[ i ] = rhs.objects[ i ];
+                objects[ i ] = rhs.objects[ i ]; // shallow copy 
             }
         }
 
         // operator = 
-        Vector &operator= ( const Vector &rhs ) {
-
-            Vector copy = rhs;
+        Vector &operator= ( Vector &rhs ) {
+            Vector<Object> copy = rhs;  // calling the copy constructor 
             std::swap( *this , copy );
             return *this;
 
@@ -39,8 +39,9 @@ class Vector{
             delete []objects;
         }
 
-
+        // copy constructor 
         Vector ( Vector && rhs ): theSize{ rhs.theSize }, theCapacity{ rhs.theCapacity } , objects{ rhs.objects }{
+            // We can do deep copy here as it is r-value reference 
             rhs.objects = nullptr;
             rhs.theSize = 0;
             rhs.theCapacity = 0;
@@ -49,7 +50,6 @@ class Vector{
 
         // operator =  // for r-value reference 
         Vector &operator= ( Vector &&rhs ) {
-
             std::swap( theSize , rhs.theSize );
             std::swap( theCapacity, rhs.theCapacity );
             std::swap( objects , rhs.objects );
@@ -79,7 +79,7 @@ class Vector{
 
             theCapacity = newCapacity;
             std::swap( objects , newArray ); // now object have a different size 
-            delete []newArray; // delete the temporary array 
+            delete []newArray; // delete the temporary array which is storing the values from previous stage
 
         }
 
@@ -125,11 +125,23 @@ class Vector{
 
         typedef const Object * const_iterator;
 
+        iterator begin( void ) {
+            return & objects[ 0 ];
+        }
 
+        const_iterator begin( void ) {
+            return & objects[ 0 ];
+        }
+
+        iterator end( void ) {
+            return &objects [ size() ];
+        } 
+
+        const_iterator( void ) {
+            return &objects[ size() ];
+        }
 };
 
-
-/***********************main function*********************************/
-int main(void){
+int main(int argc , char* argv[]){
     return 0;
 }
