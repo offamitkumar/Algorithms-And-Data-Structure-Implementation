d0bda9c116fc56230f20bf696fa77995f39abcaa
Amit Kumar <offamitkumar@gmail.com>
2020-06-11 09:49:18 +0530

    some clever code

--- mod_exponentiation.cpp
+++ mod_exponentiation.cpp
@@ -1,11 +1,11 @@
 const int MOD = (int)1e9+7;
-long long m_pow(long long a, long long b){
+long long m_pow(long long a, long long b , long long uMOD = (int)1e9+7){
     long long res{1ll};
     while(b){
         if(b&1ll){
-            res = (res*a)%MOD;
+            (res*=a)%=uMOD;
         }
-        a = (a*a)%MOD;
+        (a*=a)%=uMOD;
         b>>=1ll;
     }
     return res;
