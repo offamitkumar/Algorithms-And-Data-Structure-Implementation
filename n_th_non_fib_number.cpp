0863cba6af9a70a33684fea3bd70f9bf81359ce8
Amit Kumar <offamitkumar@gmail.com>
2020-06-20 11:58:24 +0530

    nth non fibonacci number

---
+++ n_th_non_fib_number.cpp
@@ -0,0 +1,11 @@
+int64_t n_fib(int64_t n) { 
+    int64_t prevPrev=1, prev=2, curr=3; 
+    while (n > 0) { 
+        prevPrev = prev; 
+        prev = curr; 
+        curr = prevPrev + prev; 
+        n = n - (curr - prev - 1); 
+    } 
+    n = n + (curr - prev - 1); 
+    return prev + n; 
+} 
