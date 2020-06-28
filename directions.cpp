d052cd01495a15ce3263ad2c4cd1a19d32ebd6b7
Amit Kumar <offamitkumar@gmail.com>
2020-06-28 13:54:37 +0530

    direction size added

--- directions.cpp
+++ directions.cpp
@@ -1,10 +1,12 @@
 // 8 - D
+const int DSIZE = 8;
 array<int,8>X{-1,-1,-1,0,0 ,1,1,1};
 array<int,8>Y{-1, 0, 1,1,-1,1,-1,0};
 
 
 // 4 - D
 
+const int DSIZE = 4;
 array<int,4>X{-1,1,0,0};
 array<int,4>Y{0,0,-1,1};
 
