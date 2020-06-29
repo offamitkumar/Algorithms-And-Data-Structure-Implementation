51c12359fb302838ff30b49ee96fc4efdd36a8b9
Amit Kumar <offamitkumar@gmail.com>
2020-06-29 14:11:21 +0530

    updated operator

--- matrix_exponentiation.cpp
+++ matrix_exponentiation.cpp
@@ -1,45 +1,73 @@
-
 const int MOD  = (int)1e9+7;
 
-const int MATRIX_SIZE=2;
 struct Matrix{
-    int size;
-    int64_t mat[MATRIX_SIZE][MATRIX_SIZE];
-    Matrix(int r){
-        size = r;
-        for(int i=0;i<size;++i){
-            for(int j=0;j<size;++j){
-                mat[i][j]=0ll;
+    int matrixSize;
+    vector< vector < int64_t > >mat;
+    Matrix(const int &r){
+        matrixSize = r;
+        mat.assign(r , vector<int64_t>(r, 0));
+    }
+
+    Matrix operator*(const Matrix &B){
+        Matrix T(matrixSize);
+        for ( int i = 0 ; i < matrixSize; ++i) {
+            for ( int j = 0 ; j < matrixSize; ++j) {
+                for ( int k = 0 ; k < matrixSize; ++k) {
+                    T.mat[i][j] += (mat[i][k] * B.mat[k][j]) % MOD;
+                    T.mat[i][j] %= MOD;
+                }
+            }
+        }
+        return T;
+    }
+
+    void operator*=(const Matrix &B){
+        Matrix T(matrixSize);
+        for ( int i = 0 ; i < matrixSize; ++i) {
+            for ( int j = 0 ; j < matrixSize; ++j) {
+                for ( int k = 0 ; k < matrixSize; ++k) {
+                    T.mat[i][j] += (mat[i][k] * B.mat[k][j]) % MOD;
+                    T.mat[i][j] %= MOD;
+                }
+            }
+        }
+        for (int i = 0; i < matrixSize; ++i){
+            for ( int j = 0; j < matrixSize; ++j ) {
+                mat[i][j] = T.mat[i][j];
             }
         }
     }
+
+    void makeIdentity(){
+        for ( int i = 0; i < matrixSize; ++i ) {
+            mat[i][i] = 1;
+        }
+    }
+
+    friend Matrix power(Matrix &A , int64_t b){
+        Matrix temp(A.matrixSize);
+        temp.makeIdentity();
+        while(b){
+            if(b&1){
+                temp = temp*A;
+            }
+            b>>=1;
+            A = A*A;
+        }
+        return temp;
+    }
+
+    friend ostream& operator<<(ostream&cout, const Matrix &a) {
+        for(int i=0; i < a.matrixSize; ++i) {
+            for ( int j = 0; j < a.matrixSize; ++j ) {
+                cout << a.mat[i][j] <<' ';
+            }
+            cout << '\n'; 
+        }
+        return cout;
+    }
+
 };
 
-Matrix operator*(Matrix &A , Matrix &B ){
-    Matrix T=Matrix(A.size);
-    for(int i=0;i<A.size;++i){
-        for(int j=0;j<A.size;++j){
-            for(int k=0;k<A.size;++k){
-                T.mat[i][j]+=(A.mat[i][k]*B.mat[k][j])%MOD;
-                T.mat[i][j]%=MOD;
-            }
-        }
-    }
-    return T;
-}
 
-Matrix power(Matrix A , int64_t b){
-    Matrix temp = Matrix(A.size);
-    for(int i=0;i<A.size;++i){
-        temp.mat[i][i]=1;
-    }
-    while(b){
-        if(b&1){
-            temp = temp*A;
-        }
-        b>>=1;
-        A = A*A;
-    }
-    return temp;
-}
 
