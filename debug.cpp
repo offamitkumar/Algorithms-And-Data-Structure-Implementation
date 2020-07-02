62624d6de6cfbe16b97ba3712fa96dfd91bc4a82
Amit Kumar <offamitkumar@gmail.com>
2020-07-02 20:05:48 +0530

    debug

---
+++ debug.cpp
@@ -0,0 +1,18 @@
+
+
+void Log_Gen() { cerr << endl; }
+ 
+template <typename HEAD, typename... TAIL>
+void Log_Gen(HEAD H, TAIL... T) {
+  cerr << " " << (H);
+  Log_Gen(T...);
+}
+ 
+#ifdef HELL_JUDGE
+#define Log(...) cerr << "[" << #__VA_ARGS__ << "]:", Log_Gen(__VA_ARGS__)
+#else
+#define Log(...) 0
+#endif
+
+
+
