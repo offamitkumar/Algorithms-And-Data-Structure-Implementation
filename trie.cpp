9b35023dc9615be6618407ad5dd1757da01a9277
Amit Kumar <offamitkumar@gmail.com>
2020-07-02 19:53:42 +0530

    trie ds

---
+++ trie.cpp
@@ -0,0 +1,39 @@
+
+struct Node {
+    bool isEnd;
+    Node* arr[26];
+    Node(){
+        isEnd =false;
+        for(int i=0;i<10;++i){
+            arr[i] = nullptr;
+        }
+    }
+};
+
+bool check(Node*root , const string &s){
+    Node*temp = root;
+    for(int i=0;i<(int)s.size();++i){
+        int val = s[i]-'0';
+        if(temp->arr[val] != nullptr){
+            temp = temp->arr[val];
+            if(temp->isEnd){
+                return false;
+            }
+        }else{
+            return true;
+        }
+    }
+    return false;
+}
+
+void add(Node*root , const string &s){
+    Node*temp = root;
+    for(int i=0;i<(int)s.size();++i){
+        int val = s[i]-'0';
+        if(temp->arr[val] == nullptr){
+            temp -> arr[val] = new Node;
+        }
+        temp = temp->arr[val];
+    }
+    temp->isEnd = true;
+}
