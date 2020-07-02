1aa9cc339685c23618a3bbd53374dc97db6def3f
Amit Kumar <offamitkumar@gmail.com>
2020-07-02 20:06:29 +0530

    range loops

---
+++ range.cpp
@@ -0,0 +1,24 @@
+
+class NumberIterator : iterator<forward_iterator_tag, int> {
+    public:
+        int v;
+
+        NumberIterator(int x) : v(x) {}
+
+        operator int &() { return v; }
+
+        int operator*() { return v; }
+};
+
+class range : pair<int,int> {
+    public:
+        range(int begin, int end) : pair<int,int>(begin, max(begin, end)) {}
+        range(int n) : pair<int,int>(0, max(0, n)) {}
+        NumberIterator begin() {
+            return first;
+        }
+        NumberIterator end() {
+            return second;
+        }
+};
+
