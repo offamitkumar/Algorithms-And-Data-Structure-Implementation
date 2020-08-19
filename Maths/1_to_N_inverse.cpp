56e9250127051d2735655e9309b82a098a13a4c5
Amit Kumar <offamitkumar@gmail.com>
2020-08-19 07:22:50 +0530

    updated

--- Maths/1_to_N_inverse.cpp
+++ Maths/1_to_N_inverse.cpp
@@ -1,8 +1,9 @@
-
-long long inverse[MAXN];
+/*Inverse 1 to N {{{*/
+int inverse[MAXN];
 void mod_inverse(int uMOD = (int)1e9+7){
     inverse[0] = inverse[1] = 1;
-    for ( long long i = 2ll; i < MAXN; i++ ) {
+    for ( int i = 2ll; i < MAXN; i++ ) {
         inverse[i] = inverse[uMOD % i] *  (uMOD - uMOD / i) % uMOD;
     }
 }
+/*}}}*/
